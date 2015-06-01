0d53f30387e2863a3000e56c4b072f92c08e9636
yehanzhou <yehan90717@gmail.com>
2015-06-01 15:47:24 +0800

    can run
    can run

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+var fs = require('fs');
+var accessLogfile = fs.createWriteStream('access.log', {flags: 'a'});
+var errorLogfile = fs.createWriteStream('error.log', {flags: 'a'});
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session    = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var settings = require('./settings');
+var flash = require('connect-flash');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,46 +20,84 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+app.use(favicon());
+//app.use(logger('dev'));
+app.use(logger({stream: accessLogfile}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
+
+
+//cookie解析的中间件
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
-app.use('/', routes);
-app.use('/users', users);
+//提供session支持
+app.use(session({
+  secret: settings.cookieSecret,
+  store: new MongoStore({
+      db: settings.db,
+  })
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+app.use(function(req, res, next){
+  console.log("app.usr local");
+  res.locals.user = req.session.user;
+  res.locals.post = req.session.post;
+  var error = req.flash('error');
+  res.locals.error = error.length ? error : null;
+ 
+  var success = req.flash('success');
+  res.locals.success = success.length ? success : null;
+  next();
 });
 
-// error handlers
+
+app.use('/', routes);
+if (!module.parent) {
+  app.listen(8080);
+  //console.log("Express服务器启动, 开始监听 %d 端口, 以 %s 模式运行.", app.address().port, app.settings.env);
+}
+
+app.use('/users', users);
+
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        var meta = '[' + new Date() + ']' +req.url + '\n';
+        errorLogfile.write(meta +err.stack + '\n');
+        next();
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    var meta = '[' + new Date() + ']' +req.url + '\n';
+    errorLogfile.write(meta +err.stack + '\n');
+    next();
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
